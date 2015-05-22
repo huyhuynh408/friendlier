511677e8a923571341a7c4452c2da9b8f6d05fa4
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-21 21:38:41 -0500

    added models and migration files

---
+++ db/migrate/20150522023556_create_profiles.rb
@@ -0,0 +1,17 @@
+class CreateProfiles < ActiveRecord::Migration
+  def change
+    create_table :profiles do |t|
+      t.string :age
+      t.string :height
+      t.text   :short_bio
+      t.string :interests
+      t.string :first_quality
+      t.string :second_quaity
+      t.string :third_quality
+
+      t.belongs_to :user
+
+      t.timestamps null: false
+    end
+  end
+end
