94e43eb31dbe8ff09994de89e0f04e3482dd391d
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-21 21:59:54 -0500

    updated models and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
-User.create!(name: 'Huy', email: 'huy@aol.com', friends_email: 'huy2@aol.com', password: 'huy')
+user = User.create!(name: 'Huy', email: 'huy@aol.com', friends_email: 'huy2@aol.com', password: 'huy')
+
+Profile.create!(age: 19, height: "5'1", short_bio: "A regular guy", interests: "Karate", user_id: 1)
+
+Message.create!(title: "Hello", body: "Yes!!!!", user_id: 1)
+
