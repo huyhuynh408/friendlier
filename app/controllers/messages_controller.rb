8a5472026f010a7bf2ff3bb163251f1d92b04354
Huy Huynh <huyhuynh85@gmail.com>
2015-05-23 15:11:00 -0500

    added message controller and view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,16 @@
 class MessagesController < ApplicationController
+   # before_action :authenticate_user
+   def new
+   end
+
+   def create
+     recipient = User.where(id: params['receipients'])
+     conversation = current_user.send_message
+      (
+      recipient,
+      params[:message][:body],
+      params[:message][:subject]
+      ).conversation
+      redirect_to conversation_path(conversation)
+   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ActiveRecord::Base
   has_secure_password
   acts_as_messageable
 
-  def name
-    return "Put in the name here"
-  end
+  # def name
+  #   return User.name
+  # end
 
   def mailboxer_email(object)
     #Check if an email should be sent for that object
