c6e8e1fcb8984dec69f2f2f43c9cf24850bdc7f1
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-22 19:43:58 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  resource :user, only: [:new, :create, :show]
+  resource :session, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
