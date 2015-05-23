b7915cf7b2c01a27fc9ec0ce31d009bbba41f2f1
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-23 10:26:42 -0500

    added controller and routes

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,13 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @conversations = @mailbox.inbox
+  end
+
+  private
+
+  def get_mailbox
+    @mailbox ||= current_user.mailbox
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
 
   resources :messages
-  resources :conversations
+  resources :conversations, only: [:index, :show, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
