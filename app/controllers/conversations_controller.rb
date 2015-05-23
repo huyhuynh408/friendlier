92745ff0e17c91da002b13369a8edd91f165c1d1
huyhuynh408 <huyhuynh85@gmail.com>
2015-05-23 10:47:32 -0500

    added conversations stuff

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,13 +1,14 @@
 class ConversationsController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user
 
   def index
-    @conversations = @mailbox.inbox
+    session[:user_id] = 1
+    @conversations = get_mailbox.inbox
   end
 
   private
 
   def get_mailbox
-    @mailbox ||= current_user.mailbox
+    mailbox ||= current_user.mailbox
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ActiveRecord::Base
   has_secure_password
   acts_as_messageable
 
+  def name
+    return "You should add method :name in your Messageable model"
+  end
+
+  def mailboxer_email(object)
+    #Check if an email should be sent for that object
+    #if true
+    # return "define_email@on_your.model"
+    #if false
+    return nil
+  end
+
 end
